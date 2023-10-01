0a9478186c4b520e7fc8f17a67d93656c3b4d360
Arthur-H15 <81658735+Arthur-H15@users.noreply.github.com>
2023-09-30 22:20:42 -0300

    adicionado funcoes todas as funcoes e testado

--- src/models/Iphone.java
+++ src/models/Iphone.java
@@ -6,7 +6,7 @@ import intefaces.ReprodutorMusical;
 public class Iphone  implements ReprodutorMusical{
 RepositorioMusic music;
 int idmusic=-1;
-Iphone(){
+public Iphone(){
    music= new RepositorioMusic();
    
 }
@@ -15,7 +15,11 @@ Iphone(){
         if(music.size()==0) {System.out.println("Não Existe Musicas Para Reproduzir");};
         int idDefaulth = 1;
         boolean musicRunning=idmusic>0 ;
-        int currentMusicId = musicRunning ? idmusic : idDefaulth;
+        int currentMusicId =  idmusic ;
+        if(!musicRunning){
+            idmusic = idDefaulth;
+            currentMusicId = idDefaulth;
+        }
         String currentMusic= music.getOneMusic(currentMusicId);
         System.out.println("Reposduzindo : " + currentMusic);
     }
@@ -25,19 +29,30 @@ Iphone(){
         boolean musicRunning=idmusic>0 ;
         if(!musicRunning)  System.out.println("Nao Pode Pausar Nenhuma Musica Sendo execultada No momento");
         
-        System.out.println(" pausando : " +);
+        System.out.println(" pausando : " + music.getOneMusic(idmusic));
     }
 
     @Override
     public void avancarMusica() {
+        idmusic += 1;
+        System.out.println("proxima musica");
+        this.reproduzirMusica();
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'avancarMusica'");
+        // throw new UnsupportedOperationException("Unimplemented method 'avancarMusica'");
     }
 
     @Override
     public void voltarMusica() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'voltarMusica'");
+        // // TODO Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'voltarMusica'");
+       if(idmusic>1) {
+        idmusic -= 1;
+    
+    }
+            System.out.println("musica Anterior");
+
+
+        this.reproduzirMusica();
     }
     
 }
